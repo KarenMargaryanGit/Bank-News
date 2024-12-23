0ab4a2e7e9d82653428a4cf7ab1bb598734e0c94
Karen Margaryan <137839066+KarenMargaryanGit@users.noreply.github.com>
2024-12-23 19:17:14 +0400

    Update Ineco_scraper.py

--- scraper/Ineco_scraper.py
+++ scraper/Ineco_scraper.py
@@ -5,125 +5,95 @@ import re
 import json
 from collections import OrderedDict
 from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def update_Ineco_news():
     print("--------------------------------------------------")
-
-
-    url = 'https://www.inecobank.am/api/data/Ineconews?locale=hy&type=internal&orderDirection=DESC&orderBy=Ineconews.date&offset=21&limit=10'
-
-    headers = {
-        'Cache-Control': 'no-cache',  # Avoid using cached data
-        'If-None-Match': '',  # To force the server to send fresh content
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-    }
-
-    cookies = {
-        '_ga': 'GA1.1.1533046842.1719865519', 
-        'cf_clearance': '...your_cookie_value...',
-        # Add any other relevant cookies
-    }
-
-    # Sending the GET request
-    response = requests.get(url, headers=headers, cookies=cookies)
-
-    # Output the response
-    print("Status Code:", response.status_code)
-
-
-    return
-
-
-
-
-
-
-
-
     print("Starting to update Ineco Bank news")
-    url = 'https://www.inecobank.am/api/data/Ineconews?locale=hy&orderDirection=DESC&orderBy=Ineconews.date&offset=11&limit=20'
-    # url = 'https://www.inecobank.am/hy/Individual/news/details/inecobank-your-business'
-    url1 = 'https://www.inecobank.am/hy/Individual/news/details/'
+    url_list = {'https://www.inecobank.am/hy/press-center':'news',
+                'https://www.inecobank.am/hy/press-center/announcements?category=customers':'customers',
+                'https://www.inecobank.am/hy/press-center/announcements?category=bonds':'bonds',
+                'https://www.inecobank.am/hy/press-center/announcements?category=shareholders':'shareholders',
+                'https://www.inecobank.am/hy/press-center/announcements?category=authorizedcapital':'authorizedcapital',
+                'https://www.inecobank.am/hy/press-center/announcements?category=dividends':'dividends',
+                'https://www.inecobank.am/hy/press-center/announcements?category=other2':'other',}
+    
+    url1 = 'https://www.inecobank.am'
     news_urls = []
     data = OrderedDict()
     path = 'news/ineco_news.json'
-
     
     try:
         with open(path, 'r', encoding='utf-8') as f:
             existing_data = json.load(f, object_pairs_hook=OrderedDict)
     except FileNotFoundError:
         existing_data = OrderedDict()
-
-    url = f"https://www.inecobank.am/api/data/Ineconews"
     
-    params = {
-        'locale': 'hy',
-        'orderDirection': 'DESC',
-        'orderBy': 'Ineconews.date',
-        'offset': 0,
-        'limit': 11
-    }
-    
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Connection': 'keep-alive',
-        'Referer': 'https://www.inecobank.am/',
-        'Origin': 'https://www.inecobank.am',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache',
-    }
-    with requests.Session() as session:
-            
-        response = session.get(url, headers=headers, timeout=15)
-        print(f"Status Code: {response.status_code}")
+    options = Options()
+    options.headless = True
+    options.add_argument("--headless")  # Run Chrome in headless mode
+    options.add_argument("--disable-gpu")  # Disable GPU (optional)
+    options.add_argument("--no-sandbox")  # Prevent the sandbox (optional)
 
-    # response = requests.get(url, headers=headers, params=params)
-    print(response)
-    return
-    data_ = response.json()
+    for url in url_list.keys():
+        try:
+            driver = webdriver.Chrome( options=options)
+            driver.get(url)
+            time.sleep(1)
+            page_source = driver.page_source
+            soup = BeautifulSoup(page_source, 'html.parser')
 
+            links = soup.select('div.propertyContentList__listItem div.propertyContentCard__content a.btn__link')
+            news_urls = []
+            for link in links:
+                if link.get('href'):
+                    if url1+link.get('href') in existing_data:
+                        break
+                    news_urls.append(url1+link.get('href'))
 
-    # print(len(data_))
-    for i in range(len(data_)):
-        link = f"{url1}{data_[i]['titleInUrl']}"
-        print("--------------------------------------------------")
-        print(f"Scraping URL: {link}")
+            driver.quit()
+            category = url_list[url]
+            for url in news_urls:
+                try:
+                    driver = webdriver.Chrome( options=options)
+                    print("--------------------------------------------------")
+                    print(f"Scraping URL: {url}")
+                    driver.get(url)
+                    time.sleep(1)
+                    page_source = driver.page_source
+                    soup = BeautifulSoup(page_source, 'html.parser')
 
-        if link in existing_data:
-            break
-        date = data_[i]['date'].split()[0]
-        
-        category = ''
-        title = data_[i]['headLine']
-        content = data_[i]['story']
-        summary = data_[i]['summary']
-
-        data[link] = {
-                "date": date,
-                "category": category,
-                "title": title,
-                'content': content,
-                'summary': summary,
-                "scraped_at": time.strftime("%Y-%m-%d %H:%M:%S"),
-            }
-
-        print(f"Title: {title}")
-
-        print(f"Date: {date}")
-
-        print(f"Category: {category}")
-
-        if i == 5:
-            break
-
+                    title = soup.select_one('.newsPage__title').text.strip()
+                    content = soup.select_one(".newsPage__content").text.strip()
+                    content = re.sub(r'\n+', '\n', content)
+                    content = content.replace('\r', ' ')
+                    content = content.strip()
+                    date = soup.select_one('.newsPage__date').text.split('.')
+                    formatted_date = f"{date[2]}-{date[1]}-{date[0]}"
+                    
+                    print(f"Title: {title}")
+                    print(f"Date: {formatted_date}")
+                    print(f"Category: {category}")
+                    url_entry = {
+                        "date": formatted_date,
+                        "category": category,
+                        "title": title,
+                        "content": content,
+                        "scraped_at": time.strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                    
+                    data[url] = url_entry
+                except Exception as e:
+                    print(f"Error scraping {url}: {e}")
+                    continue
+                finally:
+                    driver.quit()
+        except:
+            continue
     
     new_data_len = len(data)
     final_data = data
